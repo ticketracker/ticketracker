b54946fb263a55a071d7da8aadba47361de438eb
amirhosseinzibaei <amirhosseinzibaei@gmail.com>
2023-08-27 16:02:22 +0330

    implemented ticket class

--- core/ticket/tickets.py
+++ core/ticket/tickets.py
@@ -4,7 +4,7 @@ import datetime
 
 @dataclass
 class TicketRequest:
-	vehicle: 'Vehicle'
+	vehicle: str
 	origin_city: str
 	destination_city: str
 	passengers: dict
@@ -18,4 +18,27 @@ class TicketRequest:
 
 @dataclass
 class Ticket:
-	pass
+	departue_date: datetime.datetime
+	departue_time: datetime.time
+	avaiable_seat_count: int
+	price: int
+	description: str
+	visit_url: str
+	class_type: str
+	company_name: str
+	origin_location: str
+	destination_location: str
+
+	def to_dict(self) -> dict:
+		return {
+			"departue_date": self.departue_date,
+			"departue_time": self.departue_time,
+			"avaiable_seat_count": self.avaiable_seat_count,
+			"price": self.price,
+			"description": self.description,
+			"visit_url": self.visit_url,
+			"class_type": self.class_type,
+			"company_name": self.company_name,
+			"origin_location": self.origin_location,
+			"destination_location": self.destination_location,
+		}
