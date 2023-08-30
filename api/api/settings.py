b1863b8a2263800d9850cada84a819ceac2ca8d6
amirhosseinzibaei <amirhosseinzibaei@gmail.com>
2023-08-30 20:40:27 +0330

    changed ticketracker app name to tracker

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ticketracker.apps.TicketrackerConfig',
+    'tracker.apps.TrackerConfig',
 ]
 
 MIDDLEWARE = [
--- api/tracker/apps.py
+++ api/tracker/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TicketrackerConfig(AppConfig):
+class TrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ticketracker'
+    name = 'tracker'
